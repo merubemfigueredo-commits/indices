24b90cc830522367f9809bc7f736873850fb78b1
merubemfigueredo-commits <me.rubemfigueredo@gmail.com>
2026-08-10 08:41:36 -0300

    Update indicesfin.py

--- indicesfin.py
+++ indicesfin.py
@@ -14,7 +14,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+#from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import TimeoutException
 import os
 
